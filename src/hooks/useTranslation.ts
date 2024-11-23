d3372d9da286f85cbf904ddc9d6b94ca18f660cc
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2024-11-23 15:04:24 -0300

    refactor: create a Axios instance, configi18n and add css imports of Tailwind

---
+++ src/hooks/useTranslation.ts
@@ -0,0 +1,3 @@
+import { useTranslation as translate } from "react-i18next";
+
+export const useTranslation = (param:string[]) => translate(param);
---
+++ src/libs/axios/config.ts
@@ -0,0 +1,11 @@
+import Axios, { AxiosInstance } from "axios";
+
+const axiosInstance: AxiosInstance = Axios.create({
+  baseURL: "https://cdn-dev.preoday.com/challenge",
+  timeout: 2000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default axiosInstance;
