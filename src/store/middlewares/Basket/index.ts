e45327f5479ffd65d4e10d376dab8bbf0fd981b1
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2024-12-22 17:16:08 -0300

    fix: fix type on redux

--- src/store/middlewares/Basket/index.ts
+++ src/store/middlewares/Basket/index.ts
@@ -1,9 +1,10 @@
-import { Action, Middleware } from "@reduxjs/toolkit";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Middleware } from "@reduxjs/toolkit";
 
 import { addData } from "@src/libs/local storage";
 
 const saveToLocalStorageMiddleware: Middleware =
-  (storeAPI) => (next) => (action: Action) => {
+  (storeAPI) => (next) => (action: any) => {
     const result = next(action);
     if (action.type.startsWith("basket/")) {
       const state = storeAPI.getState();
