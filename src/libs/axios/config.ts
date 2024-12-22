c2c173a3e597a64099c37a56dfd344125b6cbc8c
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2024-12-22 20:52:58 -0300

    refactor: change proxy url on axios instance to my own proxy server

--- src/libs/axios/config.ts
+++ src/libs/axios/config.ts
@@ -1,7 +1,7 @@
 import Axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = Axios.create({
-  baseURL: " https://cors-anywhere.herokuapp.com/https://cdn-dev.preoday.com/challenge/",
+  baseURL: "https://cors-anywhere-7hvq.onrender.com/https://cdn-dev.preoday.com/challenge/",
   timeout: 2000,
 
   headers: {
