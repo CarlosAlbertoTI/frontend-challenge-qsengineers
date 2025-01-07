6ff56968cd111aeb22368d1b95bb824a6874113d
Carlos Alberto <carlosjunior.alberto4@gmail.com>
2025-01-07 11:15:03 -0300

    refactor: increase time of request with axios to 10s

--- src/libs/axios/config.ts
+++ src/libs/axios/config.ts
@@ -2,7 +2,7 @@ import Axios, { AxiosInstance } from "axios";
 
 const axiosInstance: AxiosInstance = Axios.create({
   baseURL: "https://cors-anywhere-7hvq.onrender.com/https://cdn-dev.preoday.com/challenge/",
-  timeout: 2000,
+  timeout: 10000,
 
   headers: {
     "Content-Type": "application/json",
